5d20cc38911d17eaa79bed51dcd537fc981e3129
Shaman3223 <Shashua321@gmail.com>
2025-04-02 20:38:01 -0400

    wednesday

---
+++ AddSomeNombres/AddSomeNombres/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AddSomeNombres;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Start Program ---");
+        
+        int loopAmount = 3;
+        int sum = 0;
+
+        for (int i = 0; i < loopAmount; i++)
+        {
+           int entry = 0;
+           Console.WriteLine("Please enter a number");
+                   
+           string input = Console.ReadLine();
+           entry = int.Parse(input);
+
+           sum += entry;
+        }
+        
+        Console.WriteLine("Your sum is...");
+        Console.WriteLine(sum);
+        
+        Console.WriteLine("--- End Program ---");
+    }
+}
---
+++ FirstProj1/FirstProj1/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace FirstProj1;
+// everything in C# in defined in a class
+
+struct Room
+{
+    public string nameOfRoom;
+    public string descriptionOfRoom;
+    public Dictionary<string,Action> observables;
+
+    void printDescription()
+    {
+        Console.WriteLine(descriptionOfRoom);
+    }
+
+    public Room(string name, string description, Dictionary<string,Action> keys)
+    {
+        nameOfRoom = name;
+        descriptionOfRoom = description;
+        observables = keys;
+    }
+
+}
+
+struct RoomMap
+{
+    public Room[][] mapOfRooms;
+    public int width;
+    public int height;
+
+    public RoomMap(Room[][] mapOfRooms)
+    {
+        this.mapOfRooms = mapOfRooms;
+        width = mapOfRooms[0].Length;
+        height = mapOfRooms.Length;
+    }
+
+    public Room getRoom(int x, int y)
+    {
+        return mapOfRooms[y][x];
+    }
+}
+
+class Rooms
+{
+    public static string[] Inventory = [];
+    
+    public Room lab = new Room("The Lab",
+        """
+        Its cold, it smells like gass in here. 
+        Theres a metal table in front of me with some tools. To the right is a hard glass box with a key",
+        """,
+        new Dictionary<string, Action>
+        {
+            {"table", () => { 
+                Console.WriteLine("The table is a table.");
+                if (!Inventory.Contains("hammer"))
+                {
+                    Inventory.Append("hammer");
+                } 
+            } },
+            {"tools", () => { Console.WriteLine("There is bunch of medical tools here, a scalpel, a hammer and a small box."); } },
+            {"hammer", () => { Console.WriteLine("Ive picked up the hammer!"); } }
+                
+        }
+    );
+    public Room hall = new Room("The Hallway",
+        """
+        Its even colder out here in the hallway.
+        Theres a metal table in front of me with some tools. To the right is a hard glass box with a key
+        """,
+        new Dictionary<string, Action>
+        {
+            {"table", () => { Console.WriteLine("The table is a table."); } },
+            {"tools", () => { Console.WriteLine("There is bunch of medical tools here, a scalpel, a hammer and a small box."); } },
+            {"hammer", () => { Console.WriteLine("Ive picked up the hammer!"); } }
+                
+        }
+    );
+}
+    
+
+
+class Program
+{
+    // A class is an object in coding
+    static void Main(string[] args)
+    {
+        var RCI = new Rooms(); //ROOMS CLASS INSTANCE
+
+        //This is a function named Main that performs on its own at the beginning.
+        Console.WriteLine("--- Starting Program ---");
+
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine(RCI.lab.descriptionOfRoom);
+            Console.WriteLine("Inventory: ");
+            foreach (var item in Rooms.Inventory)
+            {
+                Console.WriteLine($"- {item}");
+            }
+            
+            string input = Console.ReadLine();
+
+            foreach (var items in RCI.lab.observables)
+            {
+                if (input == items.Key)
+                {
+                    items.Value.Invoke();
+                }
+            }
+
+        }
+        
+        Console.WriteLine("--- Ending Program  ---");
+    }
+}
