cb4b9fe9f20fadb95b9fe32413c463dc85f06b12
Shaman3223 <Shashua321@gmail.com>
2025-04-03 18:35:47 -0400

    thurs

---
+++ Unit-2-Intro-To-C#/StringExample/StringExample/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace StringExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string string1 = "a";
+        string string2 = "b";
+        
+        if (string1.CompareTo(string2) > 0)
+        {
+            Console.WriteLine(" string 1 is greater than string 2");
+        }
+        else
+        {
+            Console.WriteLine(" string 1 is NOT greater than string 2");
+        }
+        
+        if (string1.CompareTo(string2) < 0)
+        {
+            Console.WriteLine(" string 1 is less than string 2");
+        }
+        else
+        {
+            Console.WriteLine(" string 1 is NOT less than string 2");
+        }
+        
+        if (string1.CompareTo(string2) == 0)
+        {
+            Console.WriteLine(" string 1 is equal than string 2");
+        }
+        else
+        {
+            Console.WriteLine(" string 1 is NOT equal than string 2");
+        }
+    }
+}
